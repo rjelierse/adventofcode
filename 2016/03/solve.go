2bd8acbd98ce304b11949cb7b1b7dde201a4c021
Raymond Jelierse <info@raymondjelierse.nl>
2018-12-03 18:31:31 +0100

    2016: solution for day 3

--- 2016/03/solve.go
+++ 2016/03/solve.go
@@ -1,13 +1,56 @@
 package day03
 
 import (
+	"fmt"
 	"github.com/google/subcommands"
 	"github.com/rjelierse/adventofcode"
+	"github.com/rjelierse/adventofcode/input"
 )
+
+func parseInput(lines []string) (values [][]int) {
+	values = make([][]int, len(lines))
+	for i, line := range lines {
+		var a, b, c int
+		if _, err := fmt.Sscanln(line, &a, &b, &c); err != nil {
+			panic(err)
+		}
+		values[i] = []int{a, b, c}
+	}
+	return
+}
+
+func part1(values [][]int) (possible int) {
+	for _, line := range values {
+		a, b, c := line[0], line[1], line[2]
+		if isTriangle(a, b, c) {
+			possible++
+		}
+	}
+	return
+}
+
+func part2(values [][]int) (possible int) {
+	for x := 0; x < 3; x++ {
+		for y := 0; y < len(values); y += 3 {
+			a, b, c := values[y][x], values[y+1][x], values[y+2][x]
+			if isTriangle(a, b, c) {
+				possible++
+			}
+		}
+	}
+	return
+}
+
+func isTriangle(a, b, c int) bool {
+	return a+b>c && a+c>b && b+c>a
+}
 
 func Command() subcommands.Command {
 	return adventofcode.NewPuzzle("day03", func(path string) error {
-	    // TODO: Implement
+		lines := input.ReadAsStrings(path)
+		values := parseInput(lines)
+	    fmt.Println("Part 1:", part1(values))
+	    fmt.Println("Part 2:", part2(values))
 		return nil
 	})
 }
